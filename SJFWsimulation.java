e3012678c152d0ab2d5f899aab7a85836d4ba208
Awruk8 <awruk8@gmail.com>
2014-03-27 18:06:55 +0100

    SJF z wywłaszczeniem
    Może ten się będzie za pierwszym razem kompilował ^^

---
+++ SJFWsimulation.java
@@ -0,0 +1,65 @@
+
+public class SJFWsimulation extends Simulation
+{
+	Process current = null;
+	
+	public SJFWsimulation(ProcessManager processManager)
+	{
+		super(processManager);
+	}
+	@Override
+	public boolean isDone() 
+	{
+		return current == null && procMan.numberRealised > 0;
+	}
+	@Override
+	public void serve() 
+	{	
+		current = nextProcess();
+		if(procMan.processList.size() > 1 && !current.isDone())
+		{
+			procMan.workTime++;
+			current.doIt();
+			procMan.overallWaited += procMan.processList.size()-1;
+			if(current.isDone())
+			{
+				procMan.processList.remove(current);
+				procMan.numberRealised++;
+			}
+		}else {
+			if(!(current == null) && !current.isDone())
+			{
+			current.doIt();
+			procMan.workTime++;
+			if(current.isDone())
+			{
+				procMan.processList.remove(current);
+				procMan.numberRealised++;
+			}
+			}
+		}
+		
+	}
+	public Process nextProcess()
+	{
+		return getShort();
+	}
+	public Process getShort()
+	{
+		int shortestTime = 100000;
+		Process shortest = null;
+		if(procMan.processList.size()>0)
+		{
+			for(int i=0; i<procMan.processList.size(); i++)
+			{
+				Process tempProc = procMan.processList.get(i);
+				if(tempProc.timeLeft<shortestTime)
+				{
+					shortestTime = tempProc.timeLeft;
+					shortest = tempProc;
+				}				
+			}
+		}
+		return shortest;
+	}
+}
